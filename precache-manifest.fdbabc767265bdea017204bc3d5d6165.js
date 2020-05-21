b706661f70cd7046e226b724094ded47a0e8bfd4
Piotr Rudzki <p.rudzki01@gmail.com>
2020-05-21 19:21:00 +0200

    Updates

---
+++ precache-manifest.fdbabc767265bdea017204bc3d5d6165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53961a340160d46da05be623c5dfc6dc",
+    "url": "/project-5-Todo-React/index.html"
+  },
+  {
+    "revision": "cc6fc5bb8995956a9b82",
+    "url": "/project-5-Todo-React/static/css/main.7aa1630d.chunk.css"
+  },
+  {
+    "revision": "4034f4cde49fe1adb7f3",
+    "url": "/project-5-Todo-React/static/js/2.ebd0afbe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-5-Todo-React/static/js/2.ebd0afbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6fc5bb8995956a9b82",
+    "url": "/project-5-Todo-React/static/js/main.2dcc0ff3.chunk.js"
+  },
+  {
+    "revision": "1e39cc317e02861fbd5e",
+    "url": "/project-5-Todo-React/static/js/runtime-main.d496484b.js"
+  }
+]);
